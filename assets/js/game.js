3c5b7950d4b65802e0d5b8d5cee167a10d786f86
dustinturp <dustinturp@outlook.com>
2021-08-13 17:50:56 -0500

    added base js

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?")
+
+console.log("Our robot's name is " + playerName);
+console.log(10 + 20)
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
